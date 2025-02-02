bb30a5acdce279ba39ab71120ed2d2607b8bae7c
mooladeepthi <mooladeepthi@yahoo.com>
2025-02-02 10:00:21 -0500

    Update

--- anvith/ImageGallery/app.js
+++ anvith/ImageGallery/app.js
@@ -19,16 +19,16 @@ let x = 0;
 prevBtn.addEventListner("click", () => {
 	x = x + 45;
 	rotate();
-})
+});
 
 homeBtn.addEventListner("click", () => {
 	<p><a href="/anvith/index.html"><button class="button">Home</button></a></p>
-})
+});
 
 nextBtn.addEventListner("click", () => {
 	x = x - 45;
 	rotate();
-})
+});
 
 function rotate(){
 	imageContainer.style.transform = 'perspective(1000px) rotateY(${x}deg)'
