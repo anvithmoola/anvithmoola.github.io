9b5171b88fd68be0ae0d019bc879833a2bd0705c
mooladeepthi <mooladeepthi@yahoo.com>
2025-03-09 17:46:27 -0400

    Update

--- anvith/diya/ImageGallery/app.js
+++ anvith/diya/ImageGallery/app.js
@@ -1,11 +1,42 @@
 //imageContainer
-const imageContainer = document.querySelector(".image-container")
+const imageContainer = document.querySelector(".image-container");
 
 //prevBtn
-const prevBtn = document.getElementById("prev")
+const prevBtn = document.getElementById("prev");
 
 //nextBtn
-const nextBtn = document.getElementById("next")
+const nextBtn = document.getElementById("next");
+
+//console.log(imageContainer);
+//console.log(prevBtn);
+//console.log(nextBtn);
+
+let x = 0;
+
+prevBtn.addEventListener("click", () => {
+  x = x + 45;
+  rotate();
+});
+
+nextBtn.addEventListener("click", () => {
+  x = x - 45;
+  rotate();
+});
+
+function rotate() {
+  imageContainer.style.transform = `perspective(1000px) rotateY(${x}deg)`;
+}
+
+/*
+
+//imageContainer
+const imageContainer = document.querySelector(".image-container");
+
+//prevBtn
+const prevBtn = document.getElementById("prev");
+
+//nextBtn
+const nextBtn = document.getElementById("next");
 
 //console.log(imageContainer);
 //console.log(prevBtn);
@@ -24,5 +55,6 @@ nextBtn.addEventListner('click', () => {
 });
 
 function rotate(){
-	imageContainer.style.transform = 'perspective(1000px) rotateY(${x}deg)'
+	imageContainer.style.transform = 'perspective(1000px) rotateY(${x}deg)';
 }
+*/
